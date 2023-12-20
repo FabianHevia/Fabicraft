c58aea223c401254fa2407084f40a936406a657d
Fabi <fabianhevia758595@gmail.com>
2023-12-19 22:36:48 -0300

    Como funciona el piso??? :,(
    Este commit es para las estadisticas la verdad, me da un error el piso :grin:

--- mineClon/src/App.jsx
+++ mineClon/src/App.jsx
@@ -2,12 +2,14 @@ import { Canvas } from '@react-three/fiber';
 import { Sky } from '@react-three/drei';
 import { Physics } from '@react-three/cannon';
 import { Ground } from './components/Ground';
+import { Camera } from './components/Camera';
 
 function App() {
   return (
         <Canvas>
           <Sky sunPosition={[100, 100, 20]} />
           <ambientLight intensity={0.5} />
+          <Camera/>
           <Physics>
             <Ground/>
           </Physics>
--- mineClon/src/components/Ground.jsx
+++ mineClon/src/components/Ground.jsx
@@ -1,14 +1,14 @@
-import { UsePlane } from '@react-three/cannon';
+import { usePlane } from '@react-three/cannon'
 
-export function Ground(){
+export function Ground() {
     const [ref] = usePlane(() => ({
-        rotation: [0, 0, 0],
+        rotation: [-Math.PI / 2, 0, 0],
         position: [0, 0, 0]
-    }));
+    }))
 
     return (
         <mesh ref={ref}>
-            <planeBufferGeometry attach="geometry" args={[100, 100]}/>
+            <planeBufferGeometry attach="geometry" args={[100, 100]} />
             <meshStandardMaterial attach='material' color='green' />
         </mesh>
     )
